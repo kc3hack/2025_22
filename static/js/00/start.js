0f89c901947b59f7aae3415c1c3b72a8f4638c34
e1q23079 <e1q23079@oit.ac.jp>
2025-02-16 16:26:06 +0900

    分割

---
+++ static/js/00/start.js
@@ -0,0 +1,11 @@
+const bgmAudio = new Audio("/static/bgm/suzukaze.mp3");  // bgm:suzukaze
+bgmAudio.volume = 0.5;
+
+function playBGM(){
+    bgmAudio.play();    // bgm:suzukazeを再生
+    bgmAudio.loop = true;   // bgmをループ
+}
+
+function startBtn(){
+    chengePlayTapSe("/play/chapter00");
+}
