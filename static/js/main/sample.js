18a46b2af861a704568fd1b3007ade0fda190bd0
e1q23079 <e1q23079@oit.ac.jp>
2025-02-17 23:08:42 +0900

    classでの実装

--- static/js/main/sample.js
+++ static/js/main/sample.js
@@ -1,5 +1,63 @@
 // main(jsOriginal)
 
+// ストーリーデータクラス
+class storySet{
+    constructor(){
+        this.storyData = [];
+        this.StoryNum = 0;
+    }
+    // 現在のストーリーを取得
+    getStory(){
+        if(this.StoryNum<this.storyData.length){
+            const newStory = this.storyData[this.StoryNum];
+            this.StoryNum++;
+            return newStory;
+        }else{
+            return null;
+        }
+    }
+    // ストーリーを設定
+    setStories(story){
+        this.storyData.push(story);
+    }
+}
+
+// ストーリー管理
+const mainStory1 = new storySet(); // メインストーリー 1
+const choice1Story = new storySet();    // 選択肢1ストーリー
+const choice2Story = new storySet();    // 選択肢2ストーリー
+const choice3Story = new storySet();    // 選択肢3ストーリー
+const mainStory2 = new storySet(); // メインストーリー2
+
+// ストーリーキー管理
+const storyKey = {"mainStory":mainStory1,"choice1Story":choice1Story,"choice2Story":choice2Story,"choice3Story":choice3Story,"mainStory2":mainStory2};
+
+// 会話クラス
+class dialog{
+    // コンストラクタ（キャラクターid,文章,保存場所）
+    constructor(characterId,word,storyId){
+        this.characterId = characterId;
+        this.word = word;
+        if(storyKey[storyId]==undefined){
+            return null;
+        }else{
+            storyKey[storyId].setStories(this);
+        }
+    }
+    // キャラクターId取得
+    getCharacterId(){
+        return this.characterId;
+    }
+    // 文章取得
+    getWord(){
+        return this.word;
+    }
+}
+
+// ストーリーを取得(.getCharacterId:.getWord)
+function getStory(storyId){
+    return storyKey[storyId].getStory();
+}
 
 
 
@@ -12,13 +70,14 @@ yoakeBGM.play();    // 再生開始
 yoakeBGM.loop = true;   // ループ
 
 // 会話内容
-const msg = [["aoi","shota"],["どこに行くのですか？","大学に行ってくる！！"]];
-let i = 0;
+new dialog("aoi","どこに行くのですか？","mainStory");
+new dialog("shota","大学に行ってくる!","mainStory");
+
 function nextBtnClick(){
-    if(i<msg.length){
-        talkChacac(msg[0][i],msg[1][i]);
+    const nowStory = getStory("mainStory");
+    if(nowStory!=null){
+        talkChacac(nowStory.getCharacterId(),nowStory.getWord());
         showBtn();
-        i++;
     }else{
         showChoice("あなたの行動を選んでください．","プログラミングする","Java演習する","C演習する");
     }
