962e5fdd5ff514d40e0b287fd77dd442e3d9c9fb
e1q23079 <e1q23079@oit.ac.jp>
2025-02-19 15:07:19 +0900

    ディレクトリ構造を変更

---
+++ static/story/chapter02/a.js
@@ -0,0 +1,31 @@
+/* ストーリー設定 */
+
+/*-----------------------編集-----------------------*/
+
+// developMent();   // 開発者モードに切り替える（ページ遷移オフ）
+// メインストーリー1
+let nowStoryName = "mainStory1";
+setBackImage("yuhi.jpg",nowStoryName);  // 画像を設定
+setPlayBgm("yoake.m4a",nowStoryName);   // BGMを設定
+setNarration("タイトル",nowStoryName);  // ナレーション
+
+// 選択肢を設定
+
+
+// 選択肢1
+nowStoryName = "choice1Story"
+
+// 選択肢2
+nowStoryName = "choice2Story";
+
+// 選択肢3
+nowStoryName = "choice3Story";
+
+// メインストーリー2
+nowStoryName = "mainStory2";
+
+
+/*-----------------------end-----------------------*/
+
+// 次のページ指定
+setNextPage("");
