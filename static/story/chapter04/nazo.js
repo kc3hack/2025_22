09048123852fcfe3862c81750ca3e48218cb12b2
e1q23079 <e1q23079@oit.ac.jp>
2025-02-21 15:18:13 +0900

    ページ遷移

--- static/story/chapter04/nazo.js
+++ static/story/chapter04/nazo.js
@@ -2,4 +2,4 @@
 showTempImage("paper.png");    // 画像を指定
 chengeBackImg("sample.png")    // 背景を指定
 setAnswerBtn("chapter04の謎","選択肢2","選択肢3","選択肢4","選択肢5",1); //答えを記述
-setNextPage("/play");   // 次のページを指定
+setNextPage("/play/chapter04/b");   // 次のページを指定
