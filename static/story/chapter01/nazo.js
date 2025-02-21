a6bcfc1761bfb21dca35fd28dcef1f0f6f0b313a
e1q23079 <e1q23079@oit.ac.jp>
2025-02-21 17:01:38 +0900

    謎解きコーナー謎見本追加

--- static/story/chapter01/nazo.js
+++ static/story/chapter01/nazo.js
@@ -1,5 +1,5 @@
 // 謎解きを記述
-showTempImage("paper.png");    // 画像を指定
-chengeBackImg("sample.png")    // 背景を指定
+showTempImage("nazo01.png");    // 画像を指定
+chengeBackImg("jinja.jpg")    // 背景を指定
 setAnswerBtn("chapter01の謎","選択肢2","選択肢3","選択肢4","選択肢5",1); //答えを記述
 setNextPage("/play/chapter01/d");   // 次のページを指定
--- static/story/chapter04/nazo02.js
+++ static/story/chapter04/nazo02.js
@@ -1,5 +1,4 @@
 // 謎解きを記述
-showTempImage("paper.png");    // 画像を指定
-chengeBackImg("sample.png")    // 背景を指定
+chengeBackImg("takedajho.png")    // 背景を指定
 setAnswerBtn("chapter04の謎","選択肢2","選択肢3","選択肢4","選択肢5",1); //答えを記述
 setNextPage("/play/chapter04/c");   // 次のページを指定
